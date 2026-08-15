efb7787f4514c2b734ec96eb11ec0404066a9d1e
Gourav <gouravrao9724@gmail.com>
2026-08-15 13:31:37 +0530

    Add API health check route

--- src/app.js
+++ src/app.js
@@ -4,8 +4,19 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 
 app.use(express.json());
+
+// Health check / home route
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Task Manager API is running',
+    status: 'success'
+  });
+});
+
+// Task API routes
 app.use('/tasks', taskRoutes);
 
+// Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Internal server error' });
